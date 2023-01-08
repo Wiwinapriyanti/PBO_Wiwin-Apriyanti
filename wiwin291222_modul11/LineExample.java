a7dc1e477260cc7defbab4745280c91d9659c18c
Wiwinapriyanti <116536834+Wiwinapriyanti@users.noreply.github.com>
2023-01-08 19:26:43 +0700

    Add files via upload

---
+++ wiwin291222_modul11/LineExample.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wiwin291222_modul11;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LineExample {
+ public static void main(String[] args){
+     Line line1 = new Line(1,4,1,1);
+     Line line2 = new Line(1,5,1,3);
+     Relation Line;
+ }   
+}
